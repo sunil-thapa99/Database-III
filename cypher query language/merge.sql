5538817968646b9c55ded7a96ebb6100fdd67501
lappanchappan43 <lappanchappan43@gmail.com>
2018-11-27 07:18:36 +0545

    MERGE COMMAND

---
+++ cypher query language/merge.sql
@@ -0,0 +1,18 @@
+MERGE (l:FOOTBALLER{name:"Sunil"}) return l
+
+-- MERGE with ON MACTH SET 
+MERGE (l:FOOTBALLER{name:'Messi'})
+ON CREATE SET l.createtime = timestamp()
+ON MATCH SET l.foundtime = timestamp()
+RETURN l.name, l.createtime, l.foundtime
+
+-- When two nodes exists but relationship doesnot exits
+CREATE (n:FOOTBALLER{name:'Ronaldo'}) RETURN n
+CREATE (m:CLUB{club_name:'Juventus'}) RETURN m
+MERGE (n:FOOTBALLER{name:'Ronaldo'})-[:PLAYS_FOR]->(m:CLUB{club_name:'Juventus'}) RETURN n, m
+
+-- WHen two nodes exists without relationship, merge a un-directional relationship
+CREATE (n:FOOTBALLER{name:'Messi'}) RETURN n;
+CREATE (m:CLUB{club_name:'FC Barcelona'}) RETURN m;
+MATCH (n:FOOTBALLER{name:"Messi"}), (m:CLUB{club_name:'FC Barcelona'})
+MERGE (n)-[:PLAYS_FOR]->(m) RETURN n, m;
